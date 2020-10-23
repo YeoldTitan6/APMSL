7dbdcf5e40a32426860768310493a7210959aea3
Yeoldtitan6 <eemuveyan@gmail.com>
2020-10-23 04:03:58 +0100

    First Update

---
+++ app/src/main/java/com/example/androidphpmysql/Constants.java
@@ -0,0 +1,8 @@
+package com.example.androidphpmysql;
+
+public class Constants {
+
+    private static final String ROOT_URL = "http://192.168.43.90/Android/v1/registerUser.php";
+
+    public static final String URL_REGISTER = ROOT_URL+"registerUser.php";
+}
